17d590104619d6434ff8226a8aed44238b6a8153
shadibdair <43513994+shadibdair@users.noreply.github.com>
2019-08-18 12:33:28 +0300

    Update Committiee SQL.sql

--- Java Projects/House committiee - Java/Committiee SQL.sql
+++ Java Projects/House committiee - Java/Committiee SQL.sql
@@ -15,9 +15,9 @@ create table Ternant(
 select * from Manager;
 
 insert into Ternant(Id,Username, Password,FirstName,LastName,ApartmentId,Payment,MonthPay)
-values(1,'A','1234','Alice','Levi',41,1200,'1 2 3 8'),
-	  (2,'B','12','Bob','Israel',1,4000,'1 2 3 4 5 6 7 8 9 10 11 12'),
-      (3,'C','345','Chris','Cohen',23,7000,'1 5 9');
+values(1,'A','1234','Shadi','Levi',41,1200,'1 2 3 8'),
+	  (2,'B','12','Bdair','Israel',1,4000,'1 2 3 4 5 6 7 8 9 10 11 12'),
+      (3,'C','345','Ronaldo','Cohen',23,7000,'1 5 9');
       
 create table Manager(
 	Id int primary key,
@@ -29,7 +29,7 @@ create table Manager(
 );
 
 insert into Manager(Id,Username, Password,FirstName,LastName,Years)
-values(1,'D','1234','John','Malka',4),
-	  (2,'E','12','Kate','Brown',2),
-      (3,'F','345','Li','Cohen',3)
+values(1,'D','1234','Ronaldinho','Malka',4),
+	  (2,'E','12','Chris','Brown',2),
+      (3,'F','345','Li','xavi',3)
       
